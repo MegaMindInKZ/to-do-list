29d31bafb3a611524fca615267dc019efc55a673
Zanggar Zhuamagaliyev <200103287@stu.sdu.edu.kz>
2022-10-17 23:04:19 +0600

    route auto changed

--- route_auth.go
+++ route_auth.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"net/http"
-	"to-do-list/data"
 )
 
 func login(writer http.ResponseWriter, request *http.Request) {
-	err := request.ParseForm()
-	user, err := data.UserByEmailOrUsername(request.PostFormValue("username-or-email"))
-	if err != nil {
-
-	}
-	if user.Password == data.Encrypt(request.PostFormValue("password")) {
-		session, err :=
-	} else {
-		http.Redirect(writer, request, "/login", 302)
-	}
+	return
+	//err := request.ParseForm()
+	//user, err := data.UserByEmailOrUsername(request.PostFormValue("username-or-email"))
+	//if err != nil {
+	//
+	//}
+	//if user.Password == data.Encrypt(request.PostFormValue("password")) {
+	//	session, err :=
+	//} else {
+	//	http.Redirect(writer, request, "/login", 302)
+	//}
 
 }

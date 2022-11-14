66dcc99ed0223cc6c7f03fc04949aa6a599de786
Zanggar Zhuamagaliyev <200103287@stu.sdu.edu.kz>
2022-11-14 21:57:01 +0600

    Receitp some methods has been added

--- data/receipt.go
+++ data/receipt.go
@@ -30,6 +30,33 @@ func (receipt *Receipt) Create() (err error) {
 	return
 }
 
+func (receipt *Receipt) Delete() (err error) {
+	st, err := DB.Prepare("DELETE FROM RECEIPT WHERE ID=$1")
+	if err != nil {
+		return
+	}
+	defer st.Close()
+	_, err = st.Exec(receipt.ID)
+	return
+}
+
+func ReceiptsByUserID(userID int) (receipts []Receipt, err error) {
+	rows, err := DB.Query("SELECT ID, USER_ID, NAME, PHOTO, INSTRUCTION FROM RECEIPTS WHERE USER_ID=$1", userID)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var receipt Receipt
+		err = rows.Scan(&receipt.ID, &receipt.User_ID, &receipt.Name, &receipt.Photo, &receipt.Instruction)
+		if err != nil {
+			return
+		}
+		receipts = append(receipts, receipt)
+	}
+	return
+}
+
 func AllReceipts() (receipts []Receipt, err error) {
 	rows, err := DB.Query("SELECT ID, USER_ID, NAME, PHOTO, INSTRUCTION FROM RECEIPTS")
 	if err != nil {
